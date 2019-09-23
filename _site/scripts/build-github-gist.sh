0d7d9465c5a2a099d2c28a591fb28897f0d00052
NikiforovAll <nikiforovalekcey@gmail.com>
2019-09-23 19:24:47 +0300

    init singleton

--- _site/scripts/build-github-gist.sh
+++ _site/scripts/build-github-gist.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pattern_name=Memento
+pattern_name=Singleton
 source_dir="C:/Nikiforov/dev/design-patterns-playground/$pattern_name/src/$pattern_name/*.cs"
 usings="/tmp/usings.$pattern_name.txt"
 code="/tmp/code.$pattern_name.cs"
